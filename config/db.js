94790e99ca0bd1ceac8152fa25dea5a7539fb470
Pranav Mahale <pranavmahale08@gmail.com>
2024-12-26 10:25:13 +0530

    Report incident image

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/emergency_alert', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect('mongodb://localhost:27017/emergency_alert');
         console.log('MongoDB connected successfully');
     } catch (err) {
         console.error('MongoDB connection failed:', err.message);
